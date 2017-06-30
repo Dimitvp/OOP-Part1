4860626838747971d775fa9c758e34918245542d
Theodor <tiapko@abv.bg>
2017-06-30 14:21:56 +0300

    01 1,2,3,4 + 02 1

---
+++ 01.Lab-DefiningClasses/StartUp.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Lab_DefiningClasses
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var clients = new Dictionary<int, BankAccount>();
+
+            var command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                var commands = command.Split();
+
+                switch (commands[0])
+                {
+                    case "Create":
+                        Create(commands[1], clients);
+                        break;
+                    case "Deposit":
+                        Deposit(commands[1], commands[2], clients);
+                        break;
+                    case "Withdraw":
+                        Withdraw(commands[1], commands[2], clients);
+                        break;
+                    case "Print":
+                        Print(commands[1], clients);
+                        break;
+
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+
+        private static void Print(string clientId, Dictionary<int, BankAccount> clients)
+        {
+            var id = int.Parse(clientId);
+
+
+            if (ChecClientExistance(clients, id))
+            {
+                Console.WriteLine($"Account ID{id}, balance {clients[id].Balance:f2}");
+            }
+        }
+
+        private static void Withdraw(string clientId, string amountToWithdraw, Dictionary<int, BankAccount> clients)
+        {
+            var id = int.Parse(clientId);
+            var amount = double.Parse(amountToWithdraw);
+
+
+            if (ChecClientExistance(clients, id))
+            {
+                if (clients[id].Balance >= amount)
+                {
+                    clients[id].Withdraw(amount);
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient balance");
+                }
+                
+
+            }
+        }
+
+        private static void Deposit(string clientId, string amountToDeposit, Dictionary<int, BankAccount> clients)
+        {
+            var id = int.Parse(clientId);
+            var amount = double.Parse(amountToDeposit);
+
+
+            if (ChecClientExistance(clients, id))
+            {
+                clients[id].Deposit(amount);
+            }
+
+        }
+
+        private static bool ChecClientExistance(Dictionary<int, BankAccount> clients, int id)
+        {
+            if (!clients.ContainsKey(id))
+            {
+                Console.WriteLine("Account does not exist");
+                return false;
+            }
+            return true;
+        }
+
+        private static void Create(string clientId, Dictionary<int, BankAccount> clients)
+        {
+            var id = int.Parse(clientId);
+
+            if (!clients.ContainsKey(id))
+            {
+                var account = new BankAccount();
+                account.ID = id;
+                clients.Add(id, account);
+            }
+            else
+            {
+                Console.WriteLine("Account already exists");
+            }
+        }
+    }
+}
---
+++ 02.Exercises-DefiningClasses/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Exercises_DefiningClasses
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            Type personType = typeof(Person);
+            FieldInfo[] fields = personType.GetFields(BindingFlags.Public | BindingFlags.Instance);
+            Console.WriteLine(fields.Length);
+
+        }
+    }
+}
